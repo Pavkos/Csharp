cd3e5986b64b60a9ec53dd7e78c9f2d70e40d9e6
DESKTOP-9MUNJB7\павел <ikorkod@gmail.com>
2017-03-27 21:33:10 +0300

    Lesson Work

---
+++ Common/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public static class  Utils
+    {
+        public static bool TryEnterNumberFromConsole(out uint number)
+        {
+            Console.Write("Введите число: ");
+            string numberAsString = Console.ReadLine();
+
+            if (!(uint.TryParse(numberAsString, out number)))
+            {
+                Console.Write("Ввели не число");
+                return false;
+            }
+
+            return true;
+        }
+    
+    }
+}
---
+++ Lesson 2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common;
+
+namespace Lesson_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint arrayLength;
+            if (!Utils.TryEnterNumberFromConsole(out arrayLength))
+            {
+                Console.Read();
+                return;
+            }
+            double[] array = new double[arrayLength];
+            Random random = new Random(100);
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.NextDouble()*100;
+            }
+            for (int j = array.Length-1; j >0; j--)
+            {
+                for (int i = 0; i <j; i++)                
+                {
+                    if (array[i] > array[i + 1])
+                    {
+                        double temp = array[i];
+                        array[i] = array[i + 1];
+                        array[i + 1] = temp;
+                    }
+                }
+
+            }
+
+            PrintArray(array);
+            Console.ReadLine();
+
+        }
+        static void PrintArray(double[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i].ToString("0.00") + " ");
+            }
+        }
+
+    }
+}
